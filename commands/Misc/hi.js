dfb4804b00da36520d92e73c28802f9b93ce907f
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-02-06 14:37:50 -0500

    Delete commands/Misc directory

--- commands/Misc/hi.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'hi',
-    description: 'Sends the word HI.',
-    execute(message, args) {
-        message.channel.send(`https://discord.com/api/oauth2/authorize?client_id=803416586282598441&permissions=8&scope=bot`);
-    },
-};
