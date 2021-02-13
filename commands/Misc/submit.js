ba5fdddc9d81b341f79e23950d777848b4a2ffaa
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-02-12 19:37:51 -0500

    Added more code

--- commands/Misc/submit.js
+++ commands/Misc/submit.js
@@ -14,10 +14,17 @@ module.exports = {
         .setThumbnail('https://w7.pngwing.com/pngs/72/540/png-transparent-no-symbol-sign-sign-stop-miscellaneous-angle-text-thumbnail.png')
         .setTitle('You can\'t submit nothing! Please provide a link.')
 
+        const error2 = new Discord.MessageEmbed()
+        .setColor(colors.red)
+        .setThumbnail('https://w7.pngwing.com/pngs/72/540/png-transparent-no-symbol-sign-sign-stop-miscellaneous-angle-text-thumbnail.png')
+        .setTitle('Submission must be a YouTube link.')
+
         if (!args[1]) {
             message.delete();
             return message.channel.send(error1);
-        }
-        message.channel.send(error1);
+        } else if (!(args[1].startsWith('https')) || !message.content.includes('youtube')) {
+            message.delete();
+            return message.channel.send(error2);
+        } else message.channel.send('Submission sent successfully!')
     }
 }
