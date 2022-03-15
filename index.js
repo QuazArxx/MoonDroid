db210050b552938da8f28ec75e579e3ecc946516
Tyler <mysubs666@gmail.com>
2022-03-14 23:13:03 -0400

    Added intents

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const fs = require('fs');
 const colors = require('./colors.json');
 const functions = require('./functions.js');
 
+const discordIntents = new Discord.Intents()
+discordIntents.add(
+    Discord.Intents.FLAGS.GUILDS,
+    Discord.Intents.FLAGS.GUILD_MEMBERS,
+    Discord.Intents.FLAGS.GUILD_PRESENCES,
+    Discord.Intents.FLAGS.GUILD_MESSAGES,
+    Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Discord.Intents.FLAGS.DIRECT_MESSAGES,
+    Discord.Intents.FLAGS.GUILD_VOICE_STATES
+)
+
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
