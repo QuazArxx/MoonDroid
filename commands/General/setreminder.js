b7ac8278a21d4ad78f3aec7aca0128095507cbe3
quazarxx <mysubs666@gmail.com>
2023-11-13 18:30:41 -0500

    Changes for update

--- commands/General/setreminder.js
+++ commands/General/setreminder.js
@@ -3,12 +3,66 @@ const { SlashCommandBuilder } = require('discord.js')
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('setreminder')
-        .setDescription('Allows users to get a DM at a specific time that is set.'),
+        .setDescription('Allows users to get a DM at a specific time that is set.')
+        .addStringOption( option =>
+            option.setName('hour')
+            .setDescription('What hour of the day do you want to receive the reminder?')
+            .setRequired(true)
+            .addChoices(
+                { name: '12am', value: 0 },
+                { name: '1am', value: 1 },
+                { name: '2am', value: 2 },
+                { name: '3am', value: 3 },
+                { name: '4am', value: 4 },
+                { name: '5am', value: 5 },
+                { name: '6am', value: 6 },
+                { name: '7am', value: 7 },
+                { name: '8am', value: 8 },
+                { name: '9am', value: 9 },
+                { name: '10am', value: 10 },
+                { name: '11am', value: 11 },
+                { name: '12pm', value: 12 },
+                { name: '1pm', value: 13 },
+                { name: '2pm', value: 14 },
+                { name: '3pm', value: 15 },
+                { name: '4pm', value: 16 },
+                { name: '5pm', value: 17 },
+                { name: '6pm', value: 18 },
+                { name: '7pm', value: 19 },
+                { name: '8pm', value: 20 },
+                { name: '9pm', value: 21 },
+                { name: '10pm', value: 22 },
+                { name: '11pm', value: 23 }
+            ))
+        .addStringOption( option => 
+            option.setName('day')
+            .setDescription('What day do you want to receive the reminder?')
+            .setRequired(true)
+            .addChoices(
+                { name: 'Sunday', value: 0 },
+                { name: 'Monday', value: 1 },
+                { name: 'Tuesday', value: 2 },
+                { name: 'Wednesday', value: 3 },
+                { name: 'Thursday', value: 4 },
+                { name: 'Friday', value: 5 },
+                { name: 'Saturday', value: 6 }
+            ))
+        .addStringOption( option =>
+            option.setName('message')
+            .setDescription('What would you like the reminder to say?')
+            .setRequired(true)),
     category: 'testing',
-    officerCommand: true,
+    officerCommand: false,
     async execute(interaction, client) {
-        // Push reminders to an array to be stored
+        let reminderHour = interaction.options.getString('hour')
+        let reminderDay = interaction.options.getString('day')
+        let reminderMessage = interaction.options.getString('message')
 
-        // In index, check for date and time for specific reminder
+        let reminder = CronJob.from({
+            cronTime: `0 0 ${reminderHour} * * ${reminderDay}`,
+        
+            onComplete: null,
+            start: false
+        })
     }
 }
--- index.js
+++ index.js
@@ -10,87 +10,13 @@ const data = require('./Data.json');
 const CronJob = require('cron').CronJob
 
 // Announces Bilefen PVP time on designated days
-const bilefenEmbed = new EmbedBuilder()
+const arenaEmbed = new EmbedBuilder()
 .setColor('#992D22')
 .setTitle('Ancient Arena [PvP] in Bilefen starts in 5 minutes!')
 
-let bilefenEvent = new CronJob('0 25 21 * * 0,2,4,6',
+let arenaEvent = new CronJob('0 25 21 * * 0,2,4,6',
     function() {
-        client.channels.cache.get(data.diabloNewsChannel).send(`<@&${data.diabloNotifRole}>`)
-        client.channels.cache.get(data.diabloNewsChannel).send({embeds: [bilefenEmbed]})
-    },
-    null,
-    true,
-    'America/New_York'
-)
-
-const demonGateEmbed = new EmbedBuilder()
-.setColor('#992D22')
-.setTitle(`Demon Gates in Realm of Damnation starts in 5 minutes!`)
-
-let demonGates1 = new CronJob("0 55 11,21 * * 1", 
-    function() {
-        client.channels.cache.get(data.diabloNewsChannel).send(`<@&${data.diabloNotifRole}>`)
-        client.channels.cache.get(data.diabloNewsChannel).send({embeds: [demonGateEmbed]})
-    },
-    null,
-    true,
-    'America/New_York'
-)
-
-let demonGates2 = new CronJob("0 25 20 * * 1", 
-    function() {
-        client.channels.cache.get(data.diabloNewsChannel).send(`<@&${data.diabloNotifRole}>`)
-        client.channels.cache.get(data.diabloNewsChannel).send({embeds: [demonGateEmbed]})
-    },
-    null,
-    true,
-    'America/New_York'
-)
-
-const hauntedCarriageEmbed = new EmbedBuilder()
-.setColor('#992D22')
-.setTitle(`Haunted Carriage in Ashwold Cemetary starts in 5 minutes!`)
-
-let hauntedCarriage1 = new CronJob("0 55 11,21 * * 2,6", 
-    function() {
-        client.channels.cache.get(data.diabloNewsChannel).send(`<@&${data.diabloNotifRole}>`)
-        client.channels.cache.get(data.diabloNewsChannel).send({embeds: [hauntedCarriageEmbed]})
-    },
-    null,
-    true,
-    'America/New_York'
-)
-
-let hauntedCarriage2 = new CronJob("0 25 20 * * 2,6", 
-    function() {
-        client.channels.cache.get(data.diabloNewsChannel).send(`<@&${data.diabloNotifRole}>`)
-        client.channels.cache.get(data.diabloNewsChannel).send({embeds: [hauntedCarriageEmbed]})
-    },
-    null,
-    true,
-    'America/New_York'
-)
-
-
-const ancientNightmareEmbed = new EmbedBuilder()
-.setColor('#992D22')
-.setTitle(`Ancient Nightmare in Mount Zavain starts in 5 minutes!`)
-
-let ancientNightmare1 = new CronJob("0 55 11,21 * * 3,5", 
-    function() {
-        client.channels.cache.get(data.diabloNewsChannel).send(`<@&${data.diabloNotifRole}>`)
-        client.channels.cache.get(data.diabloNewsChannel).send({embeds: [ancientNightmareEmbed]})
-    },
-    null,
-    true,
-    'America/New_York'
-)
-
-let ancientNightmare2 = new CronJob("0 25 20 * * 3,5", 
-    function() {
-        client.channels.cache.get(data.diabloNewsChannel).send(`<@&${data.diabloNotifRole}>`)
-        client.channels.cache.get(data.diabloNewsChannel).send({embeds: [ancientNightmareEmbed]})
+        client.channels.cache.get(data.WorldChatChannel).send({embeds: [arenaEmbed]})
     },
     null,
     true,
@@ -104,64 +30,27 @@ const assemblyEmbed = new EmbedBuilder()
 
 let assemblyEvent = new CronJob('0 55 17 * * 1-6',
     function() {
-        client.channels.cache.get(data.diabloNewsChannel).send(`<@&${data.diabloNotifRole}>`)
-        client.channels.cache.get(data.diabloNewsChannel).send({embeds: [assemblyEmbed]})
+        client.channels.cache.get(data.ClanChatChannel).send({embeds: [assemblyEmbed]})
     },
     null,
     true,
     'America/New_York'
 )
 
-// Announces Onslaught time on designated days
-const onslaughtEmbed = new EmbedBuilder()
+// Announces Shadow War time on designated days
+const warEmbed = new EmbedBuilder()
 .setColor('#992D22')
-.setTitle('Stormpoint is going to be attacked in 5 minutes!')
+.setTitle('Shadow War is about to begin! Everyone get ready to win!')
 
-let onslaught1 = new CronJob("0 55 11,21 * * 0,4",
+let warEvent = new CronJob('0 25 19 * * 4,6',
     function() {
-        client.channels.cache.get(data.diabloNewsChannel).send(`<@&${data.diabloNotifRole}>`)
-        client.channels.cache.get(data.diabloNewsChannel).send({embeds: [onslaughtEmbed]})
+        client.channels.cache.get(data.ShadowTingsChannel).send(`<@&${data.diabloClanRole}>`)
+        client.channels.cache.get(data.ShadowTingsChannel).send({embeds: [warEmbed]})
     },
     null,
     true,
     'America/New_York'
 )
-
-let onslaught2 = new CronJob("0 25 20 * * 0,4", 
-    function() {
-        client.channels.cache.get(data.diabloNewsChannel).send(`<@&${data.diabloNotifRole}>`)
-        client.channels.cache.get(data.diabloNewsChannel).send({embeds: [onslaughtEmbed]})
-    },
-    null,
-    true,
-    'America/New_York'
-)
-
-// Announces Wrathborne Invasion time on designated days
-const wrathborneEmbed = new EmbedBuilder()
-.setColor('#922D22')
-.setTitle('Get ready! A Wrathborne demon is going to invade a random zone in 5 minutes!')
-
-let wrathborne1 = new CronJob("0 55 20 * * *",
-    function() {
-        client.channels.cache.get(data.diabloNewsChannel).send(`<@&${data.diabloNotifRole}>`)
-        client.channels.cache.get(data.diabloNewsChannel).send({embeds: [wrathborneEmbed]})
-    },
-    null,
-    true,
-    'America/New_York'
-)
-
-let wrathborne2 = new CronJob("0 25 12 * * *",
-    function() {
-        client.channels.cache.get(data.diabloNewsChannel).send(`<@&${data.diabloNotifRole}>`)
-        client.channels.cache.get(data.diabloNewsChannel).send({embeds: [wrathborneEmbed]})
-    },
-    null,
-    true,
-    'America/New_York'
-)
-
 
 // Sets Discord IntentsBitField
 const discordIntents = new IntentsBitField()
@@ -195,6 +84,10 @@ client.once('ready', () => console.log('Ready!'));
 
 client.on('guildMemberAdd', async member => {
     member.roles.add('378271467969970176')
+
+    // Check if new member is a bot
+    // if not a bot, check if in database
+        // add member to database if not in
 })
 
 /*client.on('messageCreate', async message => {
